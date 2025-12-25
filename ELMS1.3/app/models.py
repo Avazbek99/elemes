f3fc94e4326a09771fc9a334da41ff7badd4e675
Avazbek <avazbektursunqulov300@gmail.com>
2025-12-26 01:22:36 +0500

    Fix: Subject.code xatolarini tuzatish
    - models.py: Subject modeliga code maydoni qo'shildi (nullable)
    - Template fayllar: subject.code ishlatilgan barcha joylarda code bo'sh bo'lganda xatolik bo'lmasligi uchun shartli tekshiruv qo'shildi
    - Barcha template fayllar yangilandi

--- ELMS1.3/app/models.py
+++ ELMS1.3/app/models.py
@@ -65,6 +65,7 @@ class Subject(db.Model):
     """Fan modeli"""
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
+    code = db.Column(db.String(20), nullable=True)  # Fan kodi (ixtiyoriy)
     description = db.Column(db.Text)
     credits = db.Column(db.Integer, default=3)  # Kredit soni
     semester = db.Column(db.Integer, default=1)  # 1-8 semestr
